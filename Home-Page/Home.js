723aaaebd1fb0e8b3e37a4bca5f280a036e0bb9b
DELL <DELL@DESKTOP-CUKKFJH>
2023-09-02 19:37:38 +0530

    Created New Project

---
+++ Home-Page/Home.js
@@ -0,0 +1,14 @@
+// Swiper Home boxes
+
+var swiper = new Swiper(".home1", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
