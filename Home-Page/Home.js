3a605f470ef21f540dd475daa296a29ad97cff14
DELL <DELL@DESKTOP-CUKKFJH>
2023-09-05 19:13:01 +0530

    Created Footer And Animated

--- Home-Page/Home.js
+++ Home-Page/Home.js
@@ -1,14 +1,84 @@
 // Swiper Home boxes
 
 var swiper = new Swiper(".home1", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+// Request for Quote (RFQ) Page
+
+const quoteText = document.querySelector(".quote"),
+  quoteBtn = document.querySelector("button"),
+  authorName = document.querySelector(".name"),
+  speechBtn = document.querySelector(".speech"),
+  copyBtn = document.querySelector(".copy"),
+  twitterBtn = document.querySelector(".twitter"),
+  synth = speechSynthesis;
+
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+  fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+    quoteText.innerText = result.content;
+    authorName.innerText = result.author;
+    quoteBtn.classList.remove("loading");
+    quoteBtn.innerText = "New Quote";
   });
+}
+
+speechBtn.addEventListener("click", () => {
+  if (!quoteBtn.classList.contains("loading")) {
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    synth.speak(utterance);
+    setInterval(() => {
+      !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
+    }, 10);
+  }
+});
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+// twitterBtn.addEventListener("click", () => {
+//   let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+//   window.open(tweetUrl, "_blank");
+// });
+
+quoteBtn.addEventListener("click", randomQuote);
+
+
+// Testimonial
+
+var swiper2 = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// Animated Scroll
+
+AOS.init({
+  duration: 3000,
+  once: true,
+});
